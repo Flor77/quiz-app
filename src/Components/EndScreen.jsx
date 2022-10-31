b8f63e41eeea3c340c62f5d3471cdfc228ca5d65
Flor77 <budaflorin@yahoo.com>
2022-10-31 20:42:05 +0200

    first commit

---
+++ src/Components/EndScreen.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useContext } from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import { Questions } from "../Helpers/QuestionBank";
+
+function EndScreen() {
+  const { score, setScore, setGameState } = useContext(QuizContext);
+
+  const restartQuiz = () => {
+    setScore(0);
+    setGameState("menu");
+  };
+  return (
+    <div className="EndScreen">
+      <h1>Quiz Finished</h1>
+      <h3>
+        {score} / {Questions.length}
+      </h3>
+      <button onClick={restartQuiz}> Restart Quiz</button>
+    </div>
+  );
+}
+
+export default EndScreen;
---
+++ src/Helpers/QuestionBank.jsx
@@ -0,0 +1,34 @@
+export const Questions = [
+  {
+    prompt: "What is my name?",
+    optionA: "John",
+    optionB: "Jake",
+    optionC: "Josh",
+    optionD: "Pedro",
+    answer: "D",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    answer: "C",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    answer: "D",
+  },
+  {
+    prompt: "Which of this is not a javascript method?",
+    optionA: "shift",
+    optionB: "push",
+    optionC: "unshift",
+    optionD: "subtract",
+    answer: "D",
+  },
+];
